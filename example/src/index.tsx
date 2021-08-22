8242fe89525e0ebb38d2acc822aac4b720861176
kaokei <kaokei.rst@gmail.com>
2021-08-22 14:01:55 +0800

    fix(reflect-metadata): import reflect-metadata

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,9 +6,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
