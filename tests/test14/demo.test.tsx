9203e248c72aa2fc5ae98cb78b21a0f41c51807d
kaokei <kaokei.rst@gmail.com>
2025-06-25 20:49:15 +0800

    完善test14

--- tests/test14/demo.test.tsx
+++ tests/test14/demo.test.tsx
@@ -1,18 +1,25 @@
-import { mount } from '@vue/test-utils';
-import DemoComp from './DemoComp.vue';
-import { DemoService } from './DemoService';
+import { render, screen, fireEvent } from '@testing-library/react';
+import DemoComp from './DemoComp';
 import { useRootService } from '@/index';
+import { DemoService } from './DemoService';
 
 describe('test14', () => {
-  it('get DemoService instance', async () => {
+  it('should render msg and count correctly, and update count on click', () => {
     const msg = 'Hello world';
-    const wrapper = mount(DemoComp, {
-      props: {
-        msg,
-      },
-    });
+    render(<DemoComp msg={msg} />);
 
-    expect(wrapper.vm.service).toBeInstanceOf(DemoService);
+    const msgNode = screen.getByTestId('msg');
+    const countNode = screen.getByTestId('count');
+    const btnCountNode = screen.getByTestId('btn-count');
+
+    expect(msgNode).toHaveExactText(msg);
+    expect(countNode).toHaveExactText('1');
+
+    fireEvent.click(btnCountNode);
+    expect(countNode).toHaveExactText('2');
+
+    fireEvent.click(btnCountNode);
+    expect(countNode).toHaveExactText('3');
 
     expect(() => {
       useRootService(DemoService);
